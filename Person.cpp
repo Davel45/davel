99cf3178dbfcc9e2602e2f646506d51569df2b58
Davel <kovalchuk2006@icloud.com>
2024-03-12 10:59:12 +0200

    add parent class

--- Person.cpp
+++ Person.cpp
@@ -1,7 +1,28 @@
 
 #include "Person.h"
 #include <iostream>
+
+using namespace std;
 Person::Person(std::string &&Name,int Age):name(std::move(Name)),age(Age){}
+void Person::set_name() {
+    cout<<"Enter name: ";
+    string name;
+    cin>>name;
+}
+void Person::get_name() {
+    cout<<"Name: "<<name<<endl;
+}
+void Person::set_age() {
+    cout<<"Enter age: ";
+    int age;
+    cin>>age;
+}
+void Person::get_age() {
+    cout<<"Age: "<<age<<endl;
+}
+void Person::info() {
+    cout<<"Name: "<<name<<"\nAge: "<<age<<endl;
+}
 Person::~Person(){
     cout<<"called Person distructor"<<endl;
 }
