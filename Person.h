f645b48d8e12e11215d106aaed7deaa732f35e11
Davel <kovalchuk2006@icloud.com>
2024-03-22 10:44:13 +0200

    add virtual function add pure virtual function add virtual destructor

--- Person.h
+++ Person.h
@@ -13,11 +13,12 @@ protected:
     string name;
     int age;
 public:
-    void set_name();
-    void get_name();
-    void set_age();
-    void get_age();
-    virtual void info();
+    virtual void set_name();
+    virtual void get_name();
+    virtual void set_age();
+    virtual void get_age();
+    virtual void info()=0;
+    virtual  void displayInfo()=0;
     Person(string &&name="None",int age=0);
     Person(const Person& other):name(other.name),age(other.age){}
     virtual ~Person();
